27c51789cda567039598f181e8a9489487e0591f
Sandy Jurek <sandyjurek@quickenloans.com>
2020-08-03 16:13:22 -0400

    Add project files.

---
+++ Lab10Homework/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lab10Homework
+{
+    public class Circle
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        private string FormatNumber(double x)
+        {
+            return Math.Round(x,2).ToString();
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+    }
+}
---
+++ Lab10Homework/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Lab10Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Circle Tester!");
+            Console.Write("Enter radius:  ");
+            var radius = double.Parse(Console.ReadLine());
+
+            //here I'm checking to see if the USER INPUT IS VALID - can't get it to work
+
+            //double isInt = Double.TryParse(radius, out isInt);
+            //if (!isInt)
+
+            //{
+            //    Console.WriteLine("Error - incorrect input - try again");
+            //    return;
+            //}
+
+            //here I'm calling the objects from the Circle.cs class
+            Circle circle1 = new Circle(radius);
+
+            //here I'm printing the circumference and area
+            Console.WriteLine($"Circumference:   {circle1.CalculateFormattedCircumference()}");
+            Console.WriteLine($"Area:  {circle1.CalculateFormattedArea()}");
+
+            //here I'm asking if they want to continue -- NEED TO FINISH THIS
+            Console.WriteLine("Continue? (y/n): ");
+
+        }
+    }
+}
